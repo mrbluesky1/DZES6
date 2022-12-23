e6b9051025864b95ce4408ed10fae9e7df1dadac
mrbluesky1 <76676130+mrbluesky1@users.noreply.github.com>
2022-12-23 23:19:19 +0600

    Add files via upload

---
+++ DZ2_Emir_21-01/main.js
@@ -0,0 +1,17 @@
+// let num = []
+// for (i = 20; i <= 20; i--) {
+//     num.push(i)
+// }
+// console.log(num)
+
+let choiceColor = prompt('Красный, желтый или зеленый').toLocaleLowerCase().trim()
+
+let color = {
+    red: 'красный';
+    yellow: 'желтый';
+    green: 'зеленый';
+}
+
+switch (choiceColor) {
+    case
+}
