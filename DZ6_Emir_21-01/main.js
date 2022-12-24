5a6f2c647f936e71be7fbf8039b823d535565da9
mrbluesky1 <76676130+mrbluesky1@users.noreply.github.com>
2022-12-24 11:12:14 +0600

    Update main.js

--- DZ6_Emir_21-01/main.js
+++ DZ6_Emir_21-01/main.js
@@ -7,9 +7,9 @@ const createTodo = function () {
         return false
     } else {
         const div = document.createElement('div')
-        let arr = []
-        arr.push(input.value)
-        arr.map((e)=>{
+        let array = []
+        array.push(input.value)
+        array.map((e)=>{
             const h2 = document.createElement('h2')
             document.body.append(h2)
             h2.append(e.split("").reverse().join(""))
